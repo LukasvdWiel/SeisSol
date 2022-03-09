18e4923f69256c96bf78242cebde9c056b9d9b90
ravil <ravil.dorozhinskii@tum.de>
2022-03-09 18:44:54 +0100

    Added additional output variables

--- src/DynamicRupture/Output/RateAndStateThermalPressurisation.hpp
+++ src/DynamicRupture/Output/RateAndStateThermalPressurisation.hpp
@@ -9,6 +9,20 @@ class RateAndStateThermalPressurisation : public RateAndState {
   using RateAndState::postCompute;
   using RateAndState::RateAndState;
   using RateAndState::tiePointers;
+
+  protected:
+  real computePf() override { return 0.0; }
+  void outputSpecifics(OutputData& outputData, size_t level, size_t receiverIdx) override {
+    auto& tpVariables = std::get<VariableID::TpVariables>(outputData.vars);
+    if (tpVariables.isActive) {
+      using DrLtsDescrT = seissol::initializers::LTS_RateAndStateThermalPressurisation;
+      auto* temperature = local.layer->var(static_cast<DrLtsDescrT*>(drDescr)->temperature);
+      tpVariables(TPID::Temperature, level, receiverIdx) = temperature[local.ltsId][local.nearestGpIndex];
+
+      auto* pressure = local.layer->var(static_cast<DrLtsDescrT*>(drDescr)->pressure);
+      tpVariables(TPID::Pressure, level, receiverIdx) = pressure[local.ltsId][local.nearestGpIndex];
+    }
+  }
 };
 } // namespace seissol::dr::output
 
